6c4f6a288d7dc03b91aeefab59f600dddca05e29
BuildTools <unconfigured@null.spigotmc.org>
2020-08-22 14:06:07 +0900

    improve building compatibility

--- src/include/racrypt.h
+++ src/include/racrypt.h
@@ -7,6 +7,8 @@
 //#define RACRYPT_AES_MIN_TABLE
 //#define RACRYPT_PRIME_MIN_TABLE
 //#define RACRYPT_RSA_VERIFY_KEY
+
+//#define WORDS_BIGENDIAN
 //#define HAVE_TIMES
 #define HAVE_STDINT_H
 
@@ -19,13 +21,13 @@ typedef          short		int16_t;
 typedef unsigned short		uint16_t;
 typedef          int		int32_t;
 typedef unsigned int		uint32_t;
-//typedef          long long	int64_t;
-//typedef unsigned long long	uint64_t;
-typedef          long		int64_t;
-typedef unsigned long		uint64_t;
 typedef          long		intptr_t;
 typedef unsigned long		uintptr_t;
+//typedef          long long	int64_t;
+//typedef unsigned long long	uint64_t;
 //#define UINT64_C(v)			v##LL
+typedef          long		int64_t;
+typedef unsigned long		uint64_t;
 #define UINT64_C(v)			v##L
 #endif
 
